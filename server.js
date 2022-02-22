bf4fbfb56a8cd65269e1ef53b5af8b708e049cf3
CodeWithVico <87899682+Codewithvico@users.noreply.github.com>
2022-02-22 12:40:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -21,8 +22,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  console.log("1", req.connection.remoteAddress);
+  res.json({ipaddress: req.connection.remoteAddress, language: req.headers['accept-language'], software: req.headers["user-agent"] });
 });
 
 
